430c82cef061edb03eab2f0e83da3050aed8b9c5
Ridvan Gundogdu <gundogdurdvan@gmail.com>
2022-07-07 22:33:46 -0500

    test

--- force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
+++ force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class TwoWayDataBinding extends LightningElement {}
+export default class TwoWayDataBinding extends LightningElement {
+fullname = "Rido";
+title = "SF Dev";
+
+handleChange(event){
+    this.title = event.target.value;
+    
+}
+}
+
